52a04609c58a539a3371d176f4bdc1e1fed0bd0d
Siddharth Shukla <siddharthshukla1996@gmail.com>
2021-09-16 16:59:42 +0530

    Update 3-data-visualisation.R

--- 3-data-visualisation.R
+++ 3-data-visualisation.R
@@ -4,16 +4,29 @@ colors()
 
 library(tidyverse)
 
+mpg
+head(mpg)
+tail(mpg)
+glimpse(mpg)
+str(mpg)
+summary(mpg)
+
+# https://stackoverflow.com/questions/24536154/what-does-mean-in-r
+# Shift + Left Mouse
+"Hello world!" %>% print()
+iris %>% head()
+iris %>% summary()
+iris %>% head() %>% summary()
+# this is equivalent to summary(head(iris))
+
 ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
 
 # ggplot(data = <DATA>) + <GEOM_FUNCTION>(mapping = aes(<MAPPINGS>))
-
 p <- ggplot(mtcars, aes(wt, mpg))
 p + geom_point()
 
 # Add aesthetic mappings
 p + geom_point(aes(colour = factor(cyl)))
-
 p + geom_point(aes(shape = factor(cyl)))
 
 ggplot(data = mpg) + 
@@ -21,7 +34,7 @@ ggplot(data = mpg) +
 # Warning message:
 # Using alpha for a discrete variable is not advised.
 
-# NOTE: '+' comes at end of line.
+# NOTE: '+' comes at end of line
 
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy, shape = class))
@@ -31,17 +44,26 @@ ggplot(data = mpg) +
 # specifying shapes manually if you must have them. 
 # 2: Removed 62 rows containing missing values (geom_point).
 
-# Correct usage.
+# Correct usage
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy), color = "blue")
 
-# Incorrect usage.
+# Incorrect usage
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy, color = "blue"))
 
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy, color = displ < 5))
 
+# Continuous variables use a color scale
+ggplot(mpg, aes(x = displ, y = hwy, colour = cty)) +
+  geom_point()
+
+# Mapping same variables to multiple aesthetics [POINTLESS]
+ggplot(mpg, aes(x = displ, y = hwy, colour = hwy, size = displ)) +
+  geom_point()
+
+?facet_wrap
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy)) + 
   facet_wrap(~ class, nrow = 2)
@@ -58,9 +80,12 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy)) +
   facet_grid(. ~ cyl)
 
-?facet_wrap
+# Faceting a continuous variable
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  facet_grid(. ~ cty)
 
-ggplot(data = mpg) + 
+ggplot(data = mpg)  
   geom_point(mapping = aes(x = displ, y = hwy)) +
   geom_smooth(mapping = aes(x = displ, y = hwy))
 
@@ -73,14 +98,18 @@ ggplot(data = mpg) +
   geom_smooth(mapping = aes(x = displ, y = hwy, linetype = drv, color = drv))
 
 # Better
-ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
-  geom_point(mapping = aes(color = drv)) +
-  geom_smooth(mapping = aes(linetype = drv, color = drv))
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = drv)) + 
+  geom_point() +
+  geom_smooth(mapping = aes(linetype = drv))
+
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = drv)) + 
+  geom_point() + 
+  geom_smooth(se = FALSE)
 
 ggplot(data = mpg) +
   geom_smooth(
     mapping = aes(x = displ, y = hwy, color = drv),
-    show.legend = TRUE
+    show.legend = FALSE
   )
 
 ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
@@ -92,6 +121,7 @@ ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
 ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + 
   geom_point(mapping = aes(color = class)) + 
   geom_smooth(se = FALSE)
+# 'se' controls display of confidence interval of standard error
 
 # geom_smooth added for sub-compact cars alone - filter()
 # Local geom_smooth data overrides global data
@@ -111,7 +141,6 @@ ggplot(mpg, aes(displ, hwy)) +
 ?gam
 
 # https://ggplot2.tidyverse.org/reference/geom_smooth.html
-# Shift + Left Mouse
 
 ?Geom
 
@@ -119,11 +148,82 @@ ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) +
   geom_point(shape = 21, colour = "white", size = 5, stroke = 3, 
              mapping = aes(fill = drv))
 
+ggplot(mtcars, aes(wt, mpg)) +
+  geom_point(shape = 21, colour = "black", fill = "white", size = 5, stroke = 5)
+
 # Bar Chart
 # Count of X plotted on Y
 # stat = "count" for geom_bar
-?geom_bar
 
+?geom_bar
 ggplot(data = diamonds) + 
   geom_bar(mapping = aes(x = cut))
+
+ggplot(data = diamonds) + 
+  stat_count(mapping = aes(x = cut))
+
+# NOTE: Every 'geom' has a default 'stat', and every 'stat' has a default 'geom'
+
+# Explicitly changing 'stat' of 'geom_bar' to '"identity"'
+?tribble # Row-wise tibble creation
+demo <- tribble(
+  ~cut,         ~freq,
+  "Fair",       1610,
+  "Good",       4906,
+  "Very Good",  12082,
+  "Premium",    13791,
+  "Ideal",      21551)
+ggplot(data = demo) +
+  geom_bar(mapping = aes(x = cut, y = freq), stat = "identity")
+
+ggplot(mpg, aes(x = class, y = drv)) +
+  geom_point()
+
+count(mpg, drv, class)
+
+# Better way of plotting two categorical variables
+ggplot(mpg, aes(x = class, y = drv)) +
+  geom_count()
+
+?geom_tile
+mpg %>%
+  count(class, drv) %>%
+  ggplot(aes(x = class, y = drv)) +
+  geom_tile(mapping = aes(fill = n))
+?geom_count
+# here 'n' is a computed variable storing number of observations at position.
+
+trees %>%
+  select(Height, Volume) %>%
+  filter(Height > 80) %>%
+  summary()
+
+ggplot(data = diamonds) + 
+  geom_bar(mapping = aes(x = cut, y = stat(prop), group = 1))
+?group # Aesthetics:grouping
+# 'prop' is a computed variable storing percent of points in that panel
+                                                          # at that position
+
+?stat_summary
+ggplot(data = diamonds) + 
+  stat_summary(
+    mapping = aes(x = cut, y = depth),
+    fun.min = min,
+    fun.max = max,
+    fun = median
+  )
+
+?complete
+?list
+mpg %>%
+  count(class, drv) %>%
+  complete(class, drv, fill = list(n = 0)) %>%
+  ggplot(aes(x = class, y = drv)) +
+  geom_tile(mapping = aes(fill = n))
+
+# line chart: geom_line()
+# boxplot: geom_boxplot()
+# histogram: geom_histogram()
+# area chart: geom_area()
+
 
